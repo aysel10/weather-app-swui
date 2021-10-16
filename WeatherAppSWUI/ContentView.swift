df030d2f3a605301f83b5c52a084f7921c01c14a
Aysel Abdullayeva <aysel2920@gmail.com>
2021-10-16 13:30:27 +0200

    Initial Commit

---
+++ WeatherAppSWUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAppSWUI
+//
+//  Created by icelkas on 16/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAppSWUI/WeatherAppSWUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSWUIApp.swift
+//  WeatherAppSWUI
+//
+//  Created by icelkas on 16/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSWUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
